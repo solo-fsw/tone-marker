888cf4dc4a9436c57b021e2ade2f137513b2fd8d
Jens van Bijsterveld <112897179+JensVanBijs@users.noreply.github.com>
2024-07-02 15:43:03 +0200

    Added bandpass filters and amplification mixers

--- software/arduino/sound-marker-v2/sound-marker-v2.ino
+++ software/arduino/sound-marker-v2/sound-marker-v2.ino
@@ -13,40 +13,72 @@
 #define STRIPS 1
 
 // GUItool: begin automatically generated code
-AudioInputI2S            i2s1;           //xy=285,359
-AudioFilterBiquad        filterMarker;   //xy=526,453
-AudioMixer4              mixerRight;     //xy=548,369
-AudioMixer4              mixerLeft;      //xy=551,280
-AudioMixer4              mixerMarker;    //xy=691,472
-AudioFilterBiquad        filterRight;    //xy=893,357
-AudioFilterBiquad        filterLeft;     //xy=895,296
-AudioAnalyzePeak         peakMarker;     //xy=1143,464
-AudioOutputI2S           i2s2;           //xy=1157,326
-AudioAnalyzeToneDetect   toneMarker3; //xy=1199,664
-AudioAnalyzeToneDetect   toneMarker4; //xy=1203,712
-AudioAnalyzeToneDetect   toneMarker2;    //xy=1204,616
-AudioAnalyzeToneDetect   toneMarker1;    //xy=1205,570
-AudioAnalyzeToneDetect   toneMarker5; //xy=1210,765
-AudioAnalyzeToneDetect   toneMarker8; //xy=1210,909
-AudioAnalyzeToneDetect   toneMarker6; //xy=1215,811
-AudioAnalyzeToneDetect   toneMarker7; //xy=1218,856
+AudioInputI2S            i2s1;           //xy=101,321
+AudioFilterBiquad        filterMarker;   //xy=346,417
+AudioMixer4              mixerLeft;      //xy=348,268
+AudioMixer4              mixerRight;     //xy=348,348
+AudioMixer4              mixerMarker;    //xy=572,436
+AudioFilterBiquad        filterRight;    //xy=580,347
+AudioFilterBiquad        filterLeft;     //xy=584,268
+AudioOutputI2S           i2s2;           //xy=781,307
+AudioAnalyzePeak         peakMarker;     //xy=945,438
+AudioFilterBiquad        filterBand1;        //xy=946,493
+AudioFilterBiquad        filterBand2; //xy=949,544
+AudioFilterBiquad        filterBand3; //xy=949,595
+AudioFilterBiquad        filterBand4; //xy=950,649
+AudioFilterBiquad        filterBand5; //xy=952,698
+AudioFilterBiquad        filterBand6; //xy=955,747
+AudioFilterBiquad        filterBand7; //xy=957,795
+AudioFilterBiquad        filterBand8; //xy=957,841
+AudioMixer4              mixerMarker1;         //xy=1261,409
+AudioMixer4              mixerMarker2; //xy=1265,497
+AudioMixer4              mixerMarker3; //xy=1273,584
+AudioMixer4              mixerMarker4; //xy=1274,664
+AudioMixer4              mixerMarker5; //xy=1274,744
+AudioMixer4              mixerMarker6; //xy=1279,828
+AudioMixer4              mixerMarker7; //xy=1280,913
+AudioMixer4              mixerMarker8; //xy=1283,988
+AudioAnalyzeToneDetect   toneMarker1;    //xy=1506,410
+AudioAnalyzeToneDetect   toneMarker2;    //xy=1510,498
+AudioAnalyzeToneDetect   toneMarker3; //xy=1512,584
+AudioAnalyzeToneDetect   toneMarker4; //xy=1512,664
+AudioAnalyzeToneDetect   toneMarker5; //xy=1514,742
+AudioAnalyzeToneDetect   toneMarker7; //xy=1516,913
+AudioAnalyzeToneDetect   toneMarker8; //xy=1518,991
+AudioAnalyzeToneDetect   toneMarker6; //xy=1519,826
 AudioConnection          patchCord1(i2s1, 0, mixerLeft, 0);
 AudioConnection          patchCord2(i2s1, 1, filterMarker, 0);
 AudioConnection          patchCord3(i2s1, 1, mixerRight, 0);
 AudioConnection          patchCord4(filterMarker, 0, mixerMarker, 0);
-AudioConnection          patchCord5(mixerRight, filterRight);
-AudioConnection          patchCord6(mixerLeft, filterLeft);
+AudioConnection          patchCord5(mixerLeft, filterLeft);
+AudioConnection          patchCord6(mixerRight, filterRight);
 AudioConnection          patchCord7(mixerMarker, peakMarker);
-AudioConnection          patchCord8(mixerMarker, toneMarker1);
-AudioConnection          patchCord9(mixerMarker, toneMarker2);
-AudioConnection          patchCord10(mixerMarker, toneMarker3);
-AudioConnection          patchCord11(mixerMarker, toneMarker4);
-AudioConnection          patchCord12(mixerMarker, toneMarker5);
-AudioConnection          patchCord13(mixerMarker, toneMarker6);
-AudioConnection          patchCord14(mixerMarker, toneMarker7);
-AudioConnection          patchCord15(mixerMarker, toneMarker8);
+AudioConnection          patchCord8(mixerMarker, filterBand1);
+AudioConnection          patchCord9(mixerMarker, filterBand2);
+AudioConnection          patchCord10(mixerMarker, filterBand3);
+AudioConnection          patchCord11(mixerMarker, filterBand4);
+AudioConnection          patchCord12(mixerMarker, filterBand5);
+AudioConnection          patchCord13(mixerMarker, filterBand6);
+AudioConnection          patchCord14(mixerMarker, filterBand7);
+AudioConnection          patchCord15(mixerMarker, filterBand8);
 AudioConnection          patchCord16(filterRight, 0, i2s2, 1);
 AudioConnection          patchCord17(filterLeft, 0, i2s2, 0);
+AudioConnection          patchCord18(filterBand1, 0, mixerMarker1, 0);
+AudioConnection          patchCord19(filterBand2, 0, mixerMarker2, 0);
+AudioConnection          patchCord20(filterBand3, 0, mixerMarker3, 0);
+AudioConnection          patchCord21(filterBand4, 0, mixerMarker4, 0);
+AudioConnection          patchCord22(filterBand5, 0, mixerMarker5, 0);
+AudioConnection          patchCord23(filterBand6, 0, mixerMarker6, 0);
+AudioConnection          patchCord24(filterBand7, 0, mixerMarker7, 0);
+AudioConnection          patchCord25(filterBand8, 0, mixerMarker8, 0);
+AudioConnection          patchCord26(mixerMarker1, toneMarker1);
+AudioConnection          patchCord27(mixerMarker2, toneMarker2);
+AudioConnection          patchCord28(mixerMarker3, toneMarker3);
+AudioConnection          patchCord29(mixerMarker4, toneMarker4);
+AudioConnection          patchCord30(mixerMarker5, toneMarker5);
+AudioConnection          patchCord31(mixerMarker6, toneMarker6);
+AudioConnection          patchCord32(mixerMarker7, toneMarker7);
+AudioConnection          patchCord33(mixerMarker8, toneMarker8);
 AudioControlSGTL5000     sgtl5000_1;     //xy=827,182
 // GUItool: end automatically generated code
 
@@ -57,28 +89,30 @@ struct BitState {
   int pinNr;
   int registerIdx;
   float frequency;
+  float gain;
+  AudioFilterBiquad *band_filter;
+  AudioMixer4 *amplifier;
   AudioAnalyzeToneDetect *freq_filter;
 };
 
 // Initialize bits
 BitState bits[8] = {
-  {0, 0, 1, 2, 16000, &toneMarker1},
-  {0, 1, 0, 3, 16500, &toneMarker2},
-  {0, 2, 24, 12, 17000, &toneMarker3},
-  {0, 3, 26, 16, 17500, &toneMarker4},
-  {0, 4, 14, 18, 20000, &toneMarker5},
-  {0, 5, 17, 22, 20000, &toneMarker6},
-  {0, 6, 16, 23, 20000, &toneMarker7},
-  {0, 7, 22, 24, 20000, &toneMarker8}
+  {0, 0, 1, 2, 16000, 1.0, &filterBand1, &mixerMarker1, &toneMarker1},
+  {0, 1, 0, 3, 16500, 1.0, &filterBand2, &mixerMarker2, &toneMarker2},
+  {0, 2, 24, 12, 17000, 1.0, &filterBand3, &mixerMarker3, &toneMarker3},
+  {0, 3, 26, 16, 17500, 1.2, &filterBand4, &mixerMarker4, &toneMarker4},
+  {0, 4, 14, 18, 20000, 1.0, &filterBand5, &mixerMarker5, &toneMarker5},
+  {0, 5, 17, 22, 20000, 1.0, &filterBand6, &mixerMarker6, &toneMarker6},
+  {0, 6, 16, 23, 20000, 1.0, &filterBand7, &mixerMarker7, &toneMarker7},
+  {0, 7, 22, 24, 20000, 1.0, &filterBand8, &mixerMarker8, &toneMarker8}
 };
-uint32_t markerRegister = 0;
 
 // Declare led strip
 CRGB leds[N_LEDS * STRIPS];
 
 // Declare global variables and constants
 elapsedMillis msecs;
-const float THRESHOLD = 0.95;  // Maximum value is correlated with the volume percentage of the connected pc
+const float AMPLITUDE_THRESHOLD = 0.95;  // Maximum value is correlated with the volume percentage of the connected pc
 const float FILTER_FREQ = 14000.0;
 const float DETECTION_THRESHOLD = 0.90;
 
@@ -108,8 +142,8 @@ void setup(){
   filterMarker.setHighpass(0, FILTER_FREQ, 10); // Leave at Q = 10
 
   // Set gains
-  mixerLeft.gain(0, 1.0);  // lowpass
-  mixerRight.gain(0, 1.0);  // lowpass
+  mixerLeft.gain(0, 0.5);  // lowpass
+  mixerRight.gain(0, 0.5);  // lowpass
   mixerMarker.gain(0, 2.0);  // highpass
 
   // Initialize led strip
@@ -120,23 +154,30 @@ void setup(){
   // Initialize marker pins and corresponding frequency filters
   for(int idx = 0; idx < 8; idx++){
     pinMode(bits[idx].pinNr, OUTPUT);
-    bits[idx].freq_filter->frequency(bits[idx].frequency, 40);
+    bits[idx].band_filter->setBandpass(0, bits[idx].frequency, 10.0);
+    bits[idx].amplifier->gain(0, bits[idx].gain);
+    bits[idx].freq_filter->frequency(bits[idx].frequency, 80);
   }
-  // toneMarker1.frequency(bits[0].frequency, 40);
 }
 
 void loop(){
   if(msecs > 50){
-    if(peakMarker.available() && peakMarker.read() > THRESHOLD){
+    if(peakMarker.available() && peakMarker.read() > AMPLITUDE_THRESHOLD){
       msecs = 0;
       for(int idx = 0; idx < 8; idx++){
         bits[idx].state = bits[idx].freq_filter->read() > DETECTION_THRESHOLD;
         GPIO6_DR = (GPIO6_DR & ~(1 << bits[idx].registerIdx)) | (bits[idx].state << bits[idx].registerIdx);
-        Serial.print(idx);
-        Serial.print("  ");
+        // Serial.print(idx);
+        // Serial.print("  ");
         // Serial.println(bits[idx].freq_filter->read());
-        Serial.println(bits[idx].state);
       }
+    }
+  }
+  if(msecs > 200){
+    msecs = 0;
+    for(int idx = 0; idx < 8; idx++){
+      bits[idx].state = 0;
+      GPIO6_DR = (GPIO6_DR & ~(1 << bits[idx].registerIdx)) | (bits[idx].state << bits[idx].registerIdx);
     }
   }
   for(int idx = 0; idx < 8; idx++){
@@ -148,13 +189,6 @@ void loop(){
   }
   FastLED.show();
 }
-
-        // markerRegister = (0x00 | m1 | (m2 << 1) | (m3 << 2) | (m4 << 3));
-        // GPIO6_DR &= 0b0000;
-        // GPIO6_DR |= markerRegister;
-
-        // fill_solid( leds, N_LEDS, CRGB::Red);
-        // FastLED.show();
 
 /*
 Usefull links:
